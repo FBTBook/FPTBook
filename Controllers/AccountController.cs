2ccb7cbc789fe2532367552f8e848f15ee8a1735
MinhThais <106128129+MinhThais@users.noreply.github.com>
2023-02-19 21:27:27 +0700

    Account Interface

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace FPTBook.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ILogger<AccountController> _db;
+
+        public AccountController(ILogger<AccountController> db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult updateAccount()
+        {
+            return View();
+        }
+        
+        public IActionResult createOwnerAccount()
+        {
+            return View();
+        }
+        
+    }
+}
