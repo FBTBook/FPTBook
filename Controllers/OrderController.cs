28cf96e3d91e685cdaed1b3f9409b9c1a6c2ad50
DKhoi02 <khoidndgcc200058@fpt.edu.vn>
2023-02-23 15:46:19 +0700

    Update manage order detail function

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -2,30 +2,50 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using LoginFPTBook.Data;
+using LoginFPTBook.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace FPTBook.Controllers
 {
     public class OrderController : Controller
     {
-        private readonly ILogger<OrderController> _db;
+        private readonly ApplicationDbContext _db;
 
-        public OrderController(ILogger<OrderController> db)
+        public OrderController(ApplicationDbContext db)
         {
             _db = db;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var orders = _db.Orders.Include(o => o.ApplicationUser).ToList();
+            return View(orders);
         }
 
-        public IActionResult orderDetail()
+        public IActionResult orderDetail(int idOrder)
         {
-            return View();
+            var orderDetails = _db.OrderDetails.Where(o => o.Order_ID == idOrder).Include(o => o.Order.ApplicationUser);
+            return View(orderDetails);
+            // ViewData["OrderDetail"] = _db.OrderDetails.Where(o => o.Order_ID == idOrder).Include(o => o.Order.ApplicationUser);
+            // return View();
         }
-        
+
+        [HttpPost]
+        public IActionResult EditOrderdetail(Order obj, string idUser, int status)
+        {
+            if(ModelState.IsValid){  
+                obj.User_ID = idUser;
+                obj.Order_Status = status;
+                _db.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("orderDetail", new {idOrder = obj.Order_ID});
+        }
     }
 }
